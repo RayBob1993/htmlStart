307b14c681f61624a416f3e8b07ad33a0aaa35e0
Turkov Dmitry <dimaxfallenangel@yandex.ru>
2021-06-14 16:01:04 +0300

    Update html.js

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,13 +1,13 @@
 const { src, dest } = require('gulp');
 const htmlPartial = require('gulp-html-partial');
-const htmlbeautify = require('gulp-html-beautify');
+const prettier = require('gulp-prettier');
 
 const config = require('../gulp.config');
 
 const htmlTask = () => {
   return src(config.path.html.dev)
     .pipe(htmlPartial(config.partials))
-    .pipe(htmlbeautify(config.htmlbeautify))
+    .pipe(prettier(config.prettier))
     .pipe(dest(config.path.html.build));
 };
 
