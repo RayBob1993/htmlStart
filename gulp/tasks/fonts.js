0df3b396342761dbd429c72f56de1108b9cdded6
Turkov Dmitry <dimaxfallenangel@yandex.ru>
2021-07-17 10:29:34 +0300

    Update fonts.js

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,9 +1,13 @@
 const { src, dest } = require('gulp');
+const cache = require('gulp-cached');
+const remember = require('gulp-remember');
 
 const config = require('../gulp.config');
 
 const fontsTask = () => {
   return src(config.path.fonts.dev)
+    .pipe(cache(config.cacheNames.fonts))
+    .pipe(remember(config.cacheNames.fonts))
     .pipe(dest(config.path.fonts.build));
 };
 
