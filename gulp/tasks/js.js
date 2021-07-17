0622c8c2620651d540cf1a0747003265f6838495
Turkov Dmitry <dimaxfallenangel@yandex.ru>
2021-07-17 10:30:11 +0300

    Update js.js

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -3,12 +3,16 @@ const uglify = require('gulp-uglify');
 const gulpIf = require('gulp-if');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
+const cache = require('gulp-cached');
+const remember = require('gulp-remember');
 
 const config = require('../gulp.config');
 const { isProd, isDev } = require('../utils/index');
 
 const jsTask = () => {
   return src(config.path.js.dev)
+    .pipe(cache(config.cacheNames.js))
+    .pipe(remember(config.cacheNames.js))
     // .pipe(gulpIf(isDev, sourcemaps.init()))
     // .pipe(gulpIf(isProd, concat(config.fileNames.js.build)))
     // .pipe(gulpIf(isProd, uglify()))
