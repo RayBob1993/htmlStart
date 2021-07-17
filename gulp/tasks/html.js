dac36cb5762b135ee7772f9552a6d13ab3c4eaf4
Turkov Dmitry <dimaxfallenangel@yandex.ru>
2021-07-17 10:29:45 +0300

    Update html.js

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,4 +1,6 @@
 const { src, dest } = require('gulp');
+const cache = require('gulp-cached');
+const remember = require('gulp-remember');
 const htmlPartial = require('gulp-html-partial');
 const prettier = require('gulp-prettier');
 
@@ -7,7 +9,9 @@ const config = require('../gulp.config');
 const htmlTask = () => {
   return src(config.path.html.dev)
     .pipe(htmlPartial(config.partials))
+    .pipe(cache(config.cacheNames.html))
     .pipe(prettier(config.prettier))
+    .pipe(remember(config.cacheNames.html))
     .pipe(dest(config.path.html.build));
 };
 
