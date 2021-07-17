b69e020bbed2644b89d57e907f4499efd6580445
Turkov Dmitry <dimaxfallenangel@yandex.ru>
2021-07-17 10:29:18 +0300

    Update watch.js

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,4 +1,7 @@
+const cache = require('gulp-cached');
+const remember = require('gulp-remember');
 const { watch, series } = require('gulp');
+
 const { htmlTask } = require('./html');
 const { imagesTask } = require('./images');
 const { stylesTask } = require('./styles');
@@ -8,13 +11,30 @@ const { reloadBrowser } = require('./server');
 
 const config = require('../gulp.config');
 
-const watchTask = (done) => {
-  watch(config.path.js.watch, series(jsTask, reloadBrowser));
-  watch(config.path.scss.watch, series(stylesTask, reloadBrowser));
-  watch(config.path.html.watch, series(htmlTask, reloadBrowser));
-  watch(config.path.images.watch, series(imagesTask, reloadBrowser));
-  watch(config.path.fonts.watch, series(fontsTask, reloadBrowser));
+function onUnlinkWatch (path, cacheName) {
+  if (path && cacheName) {
+    delete cache.caches[cacheName][path];
 
+    remember.forget(cacheName, path);
+  }
+}
+
+const watchTask = (done) => {
+  const watcherJS = watch(config.path.js.watch, series(jsTask, reloadBrowser));
+  const watcherSCSS = watch(config.path.scss.watch, series(stylesTask, reloadBrowser));
+  const watcherHTML = watch(config.path.html.watch, series(htmlTask, reloadBrowser));
+  const watcherImages = watch(config.path.images.watch, series(imagesTask, reloadBrowser));
+  const watcherFonts = watch(config.path.fonts.watch, series(fontsTask, reloadBrowser));
+
+  watcherSCSS.on('unlink', path => onUnlinkWatch(path, config.cacheNames.scss));
+
+  watcherHTML.on('unlink', path => onUnlinkWatch(path, config.cacheNames.html));
+
+  watcherJS.on('unlink', path => onUnlinkWatch(path, config.cacheNames.js));
+
+  watcherImages.on('unlink', path => onUnlinkWatch(path, config.cacheNames.images));
+
+  watcherFonts.on('unlink', path => onUnlinkWatch(path, config.cacheNames.fonts));
 
   done();
 };
